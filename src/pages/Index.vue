8a13aae81e10a967bdc59d911e43374719b133f1
Thomas Cober <tcober5@gmail.com>
2019-07-28 16:04:10 -0500

    Global styles

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,16 +1,10 @@
 <template>
   <Layout>
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
-
-    <h1>Hello, world!</h1>
-
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores</p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
+    <div class="header-image">
+      <h1>What is MNclude?</h1>
+      <p>MNclude is a safe and supportive community of individuals and peer groups in the Minnesota tech industry. Together, we are committed to taking actionable steps toward building a diverse, equitable, and inclusive local tech community.</p>
+    </div>
   </Layout>
 </template>
 
@@ -22,7 +16,20 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
+.header-image {
+  background-image: url("../../static/MNcludeHeader.png");
+  height: 300px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  color: $mn-blue;
+
+  h1,
+  p {
+    text-align: center;
+  }
+}
 .home-links a {
   margin-right: 1rem;
 }
